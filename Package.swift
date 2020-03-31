ea07b048b68a55f1bc80f1d975c3234d43874809
YOCKOW <YOCKOW@users.noreply.github.com>
2020-03-31 23:30:35 +0900

    Update dependencies to resolve bugs of spm>5.2

--- Package.swift
+++ Package.swift
@@ -11,14 +11,14 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.1"),
+    .package(url: "https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.2"),
     .package(url: "https://github.com/yaslab/CSV.swift.git", from: "2.4.2"),
-    .package(url: "https://github.com/YOCKOW/SwiftNetworkGear.git", from: "0.10.2"),
-    .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.0.2"),
-    .package(url: "https://github.com/YOCKOW/SwiftStringComposition.git", from: "1.0.0"),
-    .package(url: "https://github.com/YOCKOW/SwiftTemporaryFile.git", from: "3.2.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftNetworkGear.git", "0.11.0"..<"2.0.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftStringComposition.git", from: "1.1.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftTemporaryFile.git", from: "3.2.2"),
     .package(url: "https://github.com/YOCKOW/SwiftUnicodeSupplement.git", "0.7.1"..<"2.0.0"),
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", "0.9.0-alpha.001"..<"1.0.0"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", "0.9.0"..<"2.0.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
