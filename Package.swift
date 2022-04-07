e7b797b05865c8359d8e4d79da5d8ac459dac635
YOCKOW <YOCKOW@users.noreply.github.com>
2022-04-07 14:35:34 +0900

    Use Swift 5.6.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,19 +19,19 @@ let package = Package(
     // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/YOCKOW/SwiftBonaFideCharacterSet.git", from: "1.6.2"),
     .package(url: "https://github.com/yaslab/CSV.swift.git", from: "2.4.3"),
-    .package(url: "https://github.com/YOCKOW/SwiftNetworkGear.git", "0.14.0"..<"2.0.0"),
-    .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.1"),
-    .package(url: "https://github.com/YOCKOW/SwiftStringComposition.git", from: "2.0.0"),
-    .package(url: "https://github.com/YOCKOW/SwiftTemporaryFile.git", from: "4.0.1"),
-    .package(url: "https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "1.0.0"),
-    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.5.2"),
+    .package(url: "https://github.com/YOCKOW/SwiftNetworkGear.git", "0.14.8"..<"2.0.0"),
+    .package(url: "https://github.com/YOCKOW/SwiftRanges.git", from: "3.1.2"),
+    .package(url: "https://github.com/YOCKOW/SwiftStringComposition.git", from: "2.0.3"),
+    .package(url: "https://github.com/YOCKOW/SwiftTemporaryFile.git", from: "4.0.5"),
+    .package(url: "https://github.com/YOCKOW/SwiftUnicodeSupplement.git", from: "1.1.1"),
+    .package(url: "https://github.com/YOCKOW/ySwiftExtensions.git", from: "1.7.5"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
     .target(name: "yCodeUpdater",
             dependencies: ["SwiftBonaFideCharacterSet",
-                           "CSV",
+                           .product(name: "CSV", package: "CSV.swift"),
                            "SwiftNetworkGear",
                            "SwiftRanges",
                            "SwiftStringComposition",
@@ -41,7 +41,7 @@ let package = Package(
     ),
     .testTarget(name: "yCodeUpdaterTests",
                 dependencies: [
-                  "CSV",
+                  .product(name: "CSV", package: "CSV.swift"),
                   "SwiftNetworkGear",
                   "SwiftRanges",
                   "SwiftTemporaryFile",
@@ -57,5 +57,8 @@ if ProcessInfo.processInfo.environment["YOCKOW_USE_LOCAL_PACKAGES"] != nil {
     let dirName = url.deletingPathExtension().lastPathComponent
     return "../\(dirName)"
   }
-  package.dependencies = package.dependencies.map { .package(path: localPath(with: $0.url)) }
+  package.dependencies = package.dependencies.map {
+    guard case .sourceControl(_, let location, _) = $0.kind else { fatalError("Unexpected dependency.") }
+    return .package(path: localPath(with: location))
+  }
 }
